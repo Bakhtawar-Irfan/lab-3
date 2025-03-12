bc29d3133e2f4c1fc1546aa9d2fb1874328aefd0
Bakhtawar Irfan <Bakhtawarirfan990@gmail.com>
2025-03-12 20:04:44 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,3 @@
-//bakhtawar irfan
-//477837
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,139 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Lab#4',
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
+      title: 'Widget App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Widgets')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://images.ctfassets.net/cnu0m8re1exe/6sLnMf6GKSgsXxbJbpGXWO/7ea7a98ebc24318f0d60b1f5484053de/shutterstock_707735803.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill',
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 20),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'LAB # 4',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-
-              const SizedBox(height: 10),
-
-              // Top Image Section
-              Container(
-                width: 250,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      width: 100,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blue, width: 5),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Image.network(
-                        'https://cdn.mos.cms.futurecdn.net/Tpwmmfo3CiAJvwd4vXGzvn-800-80.jpg',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Overlapping Squares with Numbers
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(width: 80, height: 80, color: Colors.blue),
-                  Container(width: 50, height: 50, color: Colors.green),
-                  Container(
-                    width: 40,
-                    height: 240,
-                    color: Colors.red.shade700,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(
-                        9,
-                        (index) => Text(
-                          '${index + 1}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-
-              // Instruction Text
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  'PRACTICE MORE THAN THIS. IT WILL HELP YOU TO DESIGN COMPLEX MOBILE APP DESIGN',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Bottom Section
-              Container(
-                width: 250,
-                height: 80,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        'LEADING',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    ClipOval(
-                      child: Image.network(
-                        'https://png.pngtree.com/thumb_back/fh260/background/20240724/pngtree-3d-panorama-sunset-sky-hd-8k-wallpaper-stock-photographic-image-image_16097024.jpg',
-                        width: 50,
-                        height: 50,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        'TRAILING',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            children: const [
+              Icon(Icons.star, color: Colors.orange, size: 30),
+              SizedBox(width: 10),
+              Text('Flutter Widgets', style: TextStyle(fontSize: 20)),
             ],
           ),
-        ),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, child: const Text('Press Me')),
+        ],
       ),
     );
   }
